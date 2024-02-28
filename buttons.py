5d3254674945cd89b27e3b5c6ddf3708c2f869b6
Daiane Reina <daiane.veras.reina@gmail.com>
2024-02-28 16:13:37 +0000

    last modifications

--- buttons.py
+++ buttons.py
@@ -1,13 +1,20 @@
 
 from PySide6.QtWidgets import QPushButton, QWidget, QGridLayout
-
+from PySide6.QtCore import Slot
 from variables import MEDIUM_FONT_SIZE
-from utils import isNumOrDot, isEmpity
+from utils import isNumOrDot, isEmpity, isValidNumber
+from display import Display
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from display import Display
+    from info import Info
 
 
 class Button (QPushButton):
-    def __init__(self, text: str, parent: QWidget | None = None) -> None:
-        super().__init__(text, parent)
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
         self.configStyle()
 
     def configStyle(self):
@@ -16,19 +23,32 @@ class Button (QPushButton):
         self.setFont(font)
         self.setMinimumSize(75, 75)
         self.setProperty('cssClass', 'specialButton')
+        self.setCheckable(True)
 
 
 class ButtonsGrid(QGridLayout):
-    def __init__(self, parent: QWidget | None = None) -> None:
-        super().__init__(parent)
+    def __init__(self, display: 'Display', info: 'Info',  *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
         self._gridMask = [
             ['C', '+/-', '%', '÷'],
             ['7', '8', '9', 'x'],
             ['4', '5', '6', '-'],
             ['1', '2', '3', '+'],
-            ['', '0', ',', '='],
+            ['', '0', '.', '='],
         ]
+        self.display = display
+        self.info = info
+        self._equation = ''
+
+        @property
+        def equation(self):
+            return self._equation
+
+        @equation.setter
+        def equation(self, value):
+            self._equation = value
+            self.info.setText(value)
 
         self._makeGrid()
 
@@ -41,3 +61,24 @@ class ButtonsGrid(QGridLayout):
                     button.setProperty('cssClass', 'specialButton')
 
                 self.addWidget(button, rowNum, collumnNum)
+                buttonSlot = self._makeButtonDisplaySlot(
+                    self._insertButtonTextToDisplay,
+                    button
+                )
+                button.clicked.connect(buttonSlot)
+
+    def _makeButtonDisplaySlot(self, func, *args, **kwargs):
+        @Slot(bool)
+        def realSlot(_):
+            func(*args, **kwargs)
+        return realSlot
+
+    def _insertButtonTextToDisplay(self, button):
+        buttonText = button.text()
+
+        newDisplayValue = self.display.text() + buttonText
+
+        if not isValidNumber(newDisplayValue):
+            return
+
+        self.display.insert(buttonText)
--- main.py
+++ main.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     app.setWindowIcon(icon)
 
     # Info
-    info = Info('2.0 ^ 2.0 = 4.0')
+    info = Info('Sua conta')
     window.addWidgetToVLayout(info)
 
     # Display
@@ -30,7 +30,7 @@ if __name__ == '__main__':
     window.addWidgetToVLayout(display)
 
     # Grid
-    buttonsGrid = ButtonsGrid()
+    buttonsGrid = ButtonsGrid(display, info)
     window.vLayout.addLayout(buttonsGrid)
 
     # Execution
